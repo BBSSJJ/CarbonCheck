1d775637a6591b11d0a9dbd11f2d1482e13fe7d9
BBSSJJ <bdhbsj@naver.com>
2023-05-03 20:20:55 +0900

    join, login 기능 구현/ Retrofit2와 DTO를 이용하여 서버와 통신

--- app/src/main/java/kr/co/carboncheck/android/carboncheckapp/view/JoinActivity.kt
+++ app/src/main/java/kr/co/carboncheck/android/carboncheckapp/view/JoinActivity.kt
@@ -1,9 +1,17 @@
 package kr.co.carboncheck.android.carboncheckapp.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kr.co.carboncheck.android.carboncheckapp.databinding.ActivityJoinBinding
-import kr.co.carboncheck.android.carboncheckapp.databinding.ActivityLoginBinding
+import kr.co.carboncheck.android.carboncheckapp.model.JoinRequestDTO
+import kr.co.carboncheck.android.carboncheckapp.model.JoinResponseDTO
+import kr.co.carboncheck.android.carboncheckapp.network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class JoinActivity : AppCompatActivity() {
     private lateinit var binding: ActivityJoinBinding
@@ -13,6 +21,70 @@ class JoinActivity : AppCompatActivity() {
         binding = ActivityJoinBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var joinConfirmButton = binding.joinConfirmButton
+        joinConfirmButton.setOnClickListener {
+            sendJoinRequest(
+                binding.joinEmailText.text.toString(),
+                binding.joinPasswordText.text.toString(),
+                binding.joinNameText.text.toString(),
+                "local"
+            ){
+                result->
+                if(result){
+                    //회원가입 성공
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+                else{
+                    //회원가입 실패
+                }
+            }
+        }
 
+    }
+
+    private fun sendJoinRequest(
+        email: String,
+        password: String,
+        name: String,
+        authType: String,
+        callback: (Boolean) -> Unit
+    ) {
+
+        Log.d("testlog", "회원가입 요청 보냄")
+
+        val request = JoinRequestDTO(email, password, name, authType)
+        val call = RetrofitClient.service.postJoinRequest(request)
+
+        call.enqueue(object : Callback<JoinResponseDTO> {
+            override fun onResponse(
+                call: Call<JoinResponseDTO>,
+                response: Response<JoinResponseDTO>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("testlog", "회원가입 요청 응답 도착")
+                    Toast.makeText(applicationContext, response.message(), Toast.LENGTH_SHORT)
+                        .show()
+                    if (response.body()!!.success) {
+                        Log.d("testlog", "회원가입 성공")
+                        callback(true)
+                    } else {
+                        Log.d("testlog", "회원가입 실패")
+                        callback(false)
+                    }
+                } else {
+                    Log.d("testlog", "회원가입 요청 응답 도착 안함")
+                    Toast.makeText(applicationContext, response.message(), Toast.LENGTH_SHORT)
+                        .show()
+                    callback(false)
+                }
+            }
+
+            override fun onFailure(call: Call<JoinResponseDTO>, t: Throwable) {
+                Log.e("testlog", "회원가입 요청 전송 실패" + t.message)
+                Toast.makeText(applicationContext, "Request failed", Toast.LENGTH_SHORT).show()
+                callback(false)
+            }
+        })
     }
 }
--- app/src/main/java/kr/co/carboncheck/android/carboncheckapp/view/MainActivity.kt
+++ app/src/main/java/kr/co/carboncheck/android/carboncheckapp/view/MainActivity.kt
@@ -8,19 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Process
 import android.provider.Settings
-import android.util.Log
 import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kr.co.carboncheck.android.carboncheckapp.databinding.ActivityMainBinding
-import kr.co.carboncheck.android.carboncheckapp.model.MyRequest
-import kr.co.carboncheck.android.carboncheckapp.model.MyResponse
-import kr.co.carboncheck.android.carboncheckapp.network.RetrofitClient
-
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,50 +37,6 @@ class MainActivity : AppCompatActivity() {
             ).show()
             startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
         }
-
-        // 통신 제대로 되는지 확인, 버튼 누르면 데이터 전송하도록 해둠
-        val button = binding.sendButton
-        Log.d("testlog", "어플 실행")
-        button.setOnClickListener {
-            Log.d("testlog", "온클릭 리스너 적용")
-            Toast.makeText(applicationContext, "clicked!", Toast.LENGTH_SHORT).show()
-            getUserData()
-        }
-    }
-
-    // User data 가져오는 함수
-    private fun getUserData() {
-        Log.d("testlog", "데이터 보내기 시작")
-
-        // 요청 보낼 객체 생성해서 임의로 아이디 넣어줬어
-        val request = MyRequest()
-        request.userId = "tasty Lee"
-
-
-        val call = RetrofitClient.service.postData(request)
-        call.enqueue(object : Callback<MyResponse> {
-            // 전송 성공
-            override fun onResponse(call: Call<MyResponse>, response: Response<MyResponse>) {
-                if (response.isSuccessful) {
-                    // response는 서버에서 받아온 MyResponse 타입 객체입니다
-                    Log.d("testlog", "전송 성공")
-                    val userId = response.body()?.userId
-                    val usage = response.body()?.usage
-                    val time = response.body()?.time
-                    Toast.makeText(applicationContext, userId + usage + time, Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    Log.d("testlog", "전송 실패2")
-                    Toast.makeText(applicationContext, "Request failed", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            //전송 실패
-            override fun onFailure(call: Call<MyResponse>, t: Throwable) {
-                Log.e("testlog", "전송 실패1: " + t.message)
-                Toast.makeText(applicationContext, "Request failed", Toast.LENGTH_SHORT).show()
-            }
-        })
     }
 
     private fun checkForPermission(): Boolean {
