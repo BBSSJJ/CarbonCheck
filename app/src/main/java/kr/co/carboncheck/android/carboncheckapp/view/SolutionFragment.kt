7381c9807be7ece88b689e157244e7ee5159298e
BBSSJJ <bdhbsj@naver.com>
2023-06-08 15:31:54 +0900

    mission 클릭 이벤트 설정

--- app/src/main/java/kr/co/carboncheck/android/carboncheckapp/view/SolutionFragment.kt
+++ app/src/main/java/kr/co/carboncheck/android/carboncheckapp/view/SolutionFragment.kt
@@ -1,5 +1,6 @@
 package kr.co.carboncheck.android.carboncheckapp.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,7 +16,8 @@ class SolutionFragment : Fragment() {
     private val binding get() = _binding!!
     private val solutionData = mutableListOf<SolutionData>()
     private var achievedSolution = mutableListOf<Float>()
-//    private var achievedSolutionData = ArrayList<Float>(0f,0f,0f,0f)
+
+    //    private var achievedSolutionData = ArrayList<Float>(0f,0f,0f,0f)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -46,7 +48,7 @@ class SolutionFragment : Fragment() {
     }
 
     private fun initSolutionListRecyclerView() {
-        val adapter = SolutionRecyclerViewAdapter()
+        val adapter = SolutionRecyclerViewAdapter(this)
         adapter.datalist = solutionData
         binding.solutionRecyclerView.adapter = adapter
         binding.solutionRecyclerView.layoutManager = LinearLayoutManager(activity)
@@ -57,11 +59,61 @@ class SolutionFragment : Fragment() {
         with(solutionData) {
             // TODO: 여기에 실제 데이터 삽입 하시오 (미션 타이틀, 내용, 수반되는 기대효과 {수도, 전기, 비용, 탄소})
             add(SolutionData("외출시 불 끄기", "작은 실천이 큰 변화를 만듭니다", 30f, 1.2f, 160, 508.8f, false))
-            add(SolutionData("냉방 온도 1도 낮추기", "전력 사용량이 줄어듭니다", 0f, 0.31f, 40, 131f, true))
-            add(SolutionData("세탁기 절수모드로 사용하기", "수도 사용량을 줄일 수 있습니다", 30f, 0f, 300, 0.007f, true))
-            add(SolutionData("샤워시간 5분 줄이기", "수도와 전력 사용량을 동시에 줄일 수 있습니다.", 20f, 0.33f, 2300, 89.08f, false))
+            add(SolutionData("냉방 온도 1도 낮추기", "전력 사용량이 줄어듭니다", 0f, 0.31f, 40, 131f, false))
+            add(SolutionData("세탁기 절수모드로 사용하기", "수도 사용량을 줄일 수 있습니다", 30f, 0f, 300, 0.007f, false))
+            add(
+                SolutionData(
+                    "샤워시간 5분 줄이기",
+                    "수도와 전력 사용량을 동시에 줄일 수 있습니다.",
+                    20f,
+                    0.33f,
+                    2300,
+                    89.08f,
+                    false
+                )
+            )
             add(SolutionData("스마트폰 과충전 하지 않기", "스마트폰 수명이 늘어납니다.", 0f, 25.4f, 500, 508.8f, false))
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun addAchieveAmount(
+        waterUsage: Float,
+        electricityUsage: Float,
+        expenseAmount: Int,
+        carbonAmount: Float
+    ) {
+        binding.achievedWaterAmount.text = "%.1f".format(
+            (binding.achievedWaterAmount.text.toString().toFloat() + waterUsage)
+        )
+        binding.achievedElectricityAmount.text = "%.1f".format(
+            binding.achievedElectricityAmount.text.toString()
+                .toFloat() + electricityUsage
+        )
+        binding.achievedExpenseAmount.text =
+            (binding.achievedExpenseAmount.text.toString().toFloat() + expenseAmount).toInt()
+                .toString()
+        binding.achievedCarbonAmount.text =
+            "%.1f".format(binding.achievedCarbonAmount.text.toString().toFloat() + carbonAmount)
+    }
+
+    fun subAchieveAmount(
+        waterUsage: Float,
+        electricityUsage: Float,
+        expenseAmount: Int,
+        carbonAmount: Float
+    ) {
+        binding.achievedWaterAmount.text =
+            "%.1f".format(binding.achievedWaterAmount.text.toString().toFloat() - waterUsage)
+        binding.achievedElectricityAmount.text = "%.1f".format(
+            binding.achievedElectricityAmount.text.toString()
+                .toFloat() - electricityUsage
+        )
+        binding.achievedExpenseAmount.text =
+            (binding.achievedExpenseAmount.text.toString().toFloat() - expenseAmount).toInt()
+                .toString()
+        binding.achievedCarbonAmount.text =
+            "%.1f".format(binding.achievedCarbonAmount.text.toString().toFloat() - carbonAmount)
     }
 
 }
