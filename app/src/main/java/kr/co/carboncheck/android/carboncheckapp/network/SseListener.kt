81aaa1b8c83cad889f48a3ca9ccf25918889d253
BBSSJJ <bdhbsj@naver.com>
2023-06-27 22:24:32 +0900

    서버로부터 업데이트 된 사용량을 받는 SSE Listener 구현(미완)

--- app/src/main/java/kr/co/carboncheck/android/carboncheckapp/network/SseListener.kt
+++ app/src/main/java/kr/co/carboncheck/android/carboncheckapp/network/SseListener.kt
@@ -2,6 +2,7 @@ package kr.co.carboncheck.android.carboncheckapp.network
 
 import android.app.ProgressDialog
 import android.util.Log
+import androidx.room.Update
 import com.google.gson.Gson
 import okhttp3.Response
 import okhttp3.sse.EventSource
@@ -9,7 +10,8 @@ import okhttp3.sse.EventSourceListener
 
 class SseListener : EventSourceListener() {
     private val gson = Gson()
-//    private var progressDialog: ProgressDialog = null
+
+    //    private var progressDialog: ProgressDialog = null
     override fun onOpen(eventSource: EventSource, response: Response) {
         super.onOpen(eventSource, response)
         Log.d("testlog", "SSE연결이 열렸습니다")
@@ -24,10 +26,26 @@ class SseListener : EventSourceListener() {
     override fun onEvent(eventSource: EventSource, id: String?, type: String?, data: String) {
         super.onEvent(eventSource, id, type, data)
         Log.d("testlog", data)
-        val jsonData = gson.fromJson(data, SseDto::class.java)
-        if(jsonData.msg == "register_finish"){
+
+        if (id == "update_usage") {
+            if(type == "electricity_usage") {
+                val receivedData = gson.fromJson(data, UpdateElectricityUsageDto::class.java)
+
+            }
+            else if(type == "water_usage"){
+                val receivedData = gson.fromJson(data, UpdateWaterUsageDto::class.java)
+            }
+
+        } else {
+            val receivedData = gson.fromJson(data, SseDto::class.java)
+            if (receivedData.msg == "register_finish") {
 //            progressDialog?.dismiss()
+                //창 닫도록
+            }
         }
+
+
+
     }
 
     override fun onFailure(eventSource: EventSource, t: Throwable?, response: Response?) {
---
+++ app/src/main/java/kr/co/carboncheck/android/carboncheckapp/network/UpdateElectricityUsageDto.kt
@@ -0,0 +1,6 @@
+package kr.co.carboncheck.android.carboncheckapp.network
+
+data class UpdateElectricityUsageDto(
+    val plugId: String,
+    val amount: Float
+)
